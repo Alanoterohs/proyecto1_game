f858b04b08ab169f8887257ae8ce474746b21c71
Alanoterohs <alanoterohs@gmail.com>
2021-06-09 19:57:05 -0300

    use conventions

--- game_2.py
+++ game_2.py
@@ -1,130 +1,123 @@
-# Por último, se pide elaborar y mostrar las siguientes estadísticas:
-
-# La cantidad de jugadas realizadas (recordando que una jugada consiste en los turnos de ambos jugadores).
-# Si hubo al menos una jugada con puntaje empatado entre ambos jugadores.
-# El puntaje promedio obtenido por jugada por cada jugador.
-# El porcentaje de aciertos para cada jugador (considerando acierto si la suma de los dados coincidió con la paridad
-# apostada). Indicar también si el ganador es el que tuvo mayor porcentaje de aciertos.
-# Si algún jugador ganó en al menos 3 turnos seguidos.
-
 import random
 
 player1 = input('\nNombre del primer jugador: ')
 player2 = input('\nNombre del segundo jugador: ')
-maxScore1 = maxScore2 = 0
-rounds = roundsWon1 = roundsWon2 = roundsTied = 0
-average = hitPlayer1 = hitPlayer2 = percentagePlayer1 = percentagePlayer2 = 0
-objetivo = int(input('Puntaje a alcanzar(mayor que 10, por favor): '))
 
-if objetivo < 10:
+max_score_player1 = max_score_player2 = 0 # accumulate points for each player
+rounds = rounds_won_player1 = rounds_won_player2 = rounds_tied = 0 #total rounds/ rounds won for each player/ rounds tied
+hit_player1 = hit_player2 = percentage_player1 = percentage_player2 = 0
+accumulator_player1 = accumulator_player2 = 0
+
+target = int(input('Puntaje a alcanzar(mayor que 10, por favor): '))
+
+if target < 10:
     print("ERROR Ingrese un valor mayor a 10")
-    objetivo = int(input('Puntaje a alcanzar: (mayor que 10, por favor) '))
+    target = int(input('Puntaje a alcanzar: (mayor que 10, por favor) '))
 
-def tiradaDados():
+def throw_dice():
     a = random.randint(1, 6)
     b = random.randint(1, 6)
     c = random.randint(1, 6)
     print('valor de los dados: (',a, ',', b, ',', c,')')
     return a, b, c
 
-def turno(player):
+def turn(player):
     print("\n\nEs el turno de", player)
     print("¿Cuál crees que será la paridad de la suma de los dados de tu próxima tirada?")
-    print("Si aciertas, a tu puntaje sumaras el valor del mayor dado \nDe lo contrario se le restara el valor del menor")
 
-    apuesta = int(input("\nPulsa 1 seguido de Enter para apostar por impar\
+    bet = int(input("\nPulsa 1 seguido de Enter para apostar por impar\
     \nPulsa 2 seguido de Enter para apostar por par\n"))
-    assert apuesta == 1 or apuesta == 2
+    assert bet == 1 or bet == 2
     input("\n*** Presiona enter para lanzar los dados ***")
-    return apuesta
+    return bet
 
-def par_impar(a,b,c, apuesta):
+def even_odd(a, b, c, bet):
     hit = 0
-    if (a + b + c) % 2 == 0 and apuesta == 2:
+    if (a + b + c) % 2 == 0 and bet == 2:
         hit+=1
-    elif (a + b + c) % 2 != 0 and apuesta == 1:
+    elif (a + b + c) % 2 != 0 and bet == 1:
         hit+=1
     return hit
 
-def jugada(a, b, c, apuesta):
-    dadoMayor = max(a, b, c)
-    dadoMenor = min(a, b, c)
+def jugada(a, b, c, bet):
+    major_dice = max(a, b, c)
+    minor_die  = min(a, b, c)
     score = 0
 
     if (a + b + c) % 2 == 0:
-        if apuesta == 2:
-            score = dadoMayor
-            print("¡¡¡Felicidades!!! \nHaz acertado en tu predicción")
+        if bet == 2:
+            score = major_dice
             if a % 2 == 0 and b % 2 == 0 and c % 2 == 0:
-                score = dadoMayor*2
-                print("\nBONUS por paridad apostada en cada dado \n¡Puntaje DOBLE!")
+                score = major_dice*2
         else:
-            score -= dadoMenor
-            print("Por poco lo logras \nMejor suerte la proxima")
-    elif apuesta == 1:
-        score = dadoMayor
-        print("¡¡¡Felicidades!!! \nHaz acertado en tu predicción")
+            score -= minor_die
+    elif bet == 1:
+        score = major_dice
         if a % 2 != 0 and b % 2 != 0 and c % 2 != 0:
-            score = dadoMayor*2
-            print("\nBONUS por paridad apostada en cada dado \n¡Puntaje DOBLE!")
+            score = major_dice*2
     else:
-        score -= dadoMenor
-        print("Por poco lo logras \nMejor suerte la proxima")
+        score -= minor_die
 
     return score
 
-# correntScore = jugada(a, b, c, apuesta)
-# print("\n\nPuntaje de este turno:", correntScore)
+while max_score_player1 < target and max_score_player2 < target: #target score
+    bet = turn(player1)
+    a, b, c = throw_dice()
+    hit_player1 += even_odd(a, b, c, bet)
+    accumulator_player1 = jugada(a, b, c, bet)
+    max_score_player1 = max_score_player1 + accumulator_player1
+    print("\n\nPuntaje de este turno:", accumulator_player2)
 
+    bet = turn(player2)
+    a, b, c = throw_dice()
+    hit_player2 += even_odd(a, b, c, bet)
+    accumulator_player2 += jugada(a, b, c, bet)
+    max_score_player2 = max_score_player2 + accumulator_player2
+    print("\n\nPuntaje de este turno:", accumulator_player2)
 
-while maxScore1 < objetivo and maxScore2 < objetivo:
-    apuesta = turno(player1)
-    a, b, c = tiradaDados()
-    hitPlayer1 += par_impar(a, b, c, apuesta)
-    maxScore1 += jugada(a, b, c, apuesta)
-    eachScore1 = jugada(a, b, c, apuesta)
-    print("\n\nPuntaje de este turno:", eachScore1)
-
-    apuesta = turno(player2)
-    a, b, c = tiradaDados()
-    hitPlayer2 += par_impar(a, b, c, apuesta)
-    maxScore2 += jugada(a, b, c, apuesta)
-    eachScore2 = jugada(a, b, c, apuesta)
-    print("\n\nPuntaje de este turno:", eachScore2)
-
+    #save total rounds
     rounds+= 1
-
-    if eachScore1 > eachScore2:
-        roundsWon1 = roundsWon1 + 1
-    elif eachScore2 > eachScore1:
-        roundsWon1 = roundsWon1 + 1
+    #save rounds won
+    if accumulator_player1 > accumulator_player2:
+        rounds_won_player1 = rounds_won_player1 + 1
+    elif accumulator_player2 > accumulator_player1:
+        rounds_won_player1 = rounds_won_player1 + 1
     else:
-        roundsTied = roundsTied + 1
+        rounds_tied = rounds_tied + 1 #save rounds tied
+
+##************************************ END ******************************************************
+
+print('\n*** FIN DEL JUEGO ***')
+
+#Show score and name
+print("\n\nEl puntaje del jugador", player1, ' es: ', max_score_player1)
+print("\nEl puntaje del jugador", player2, ' es: ', max_score_player2, '\n')
+
+print('***** ESTADISTICAS *****')
+
+# El porcentaje de aciertos para cada jugador (considerando acierto si la suma de los dados coincidió con la paridad
+percentage_player1 = (hit_player1 * 100 // rounds)
+percentage_player2 = (hit_player2 * 100 // rounds)
 
 
-print("\n\nEl puntaje del jugador:", player1, ' es: ', maxScore1)
-print("\nEl puntaje del jugador:", player2, ' es: ', maxScore2, '\n')
-
-percentagePlayer1 = (hitPlayer1 * 100 // rounds)
-percentagePlayer2 = (hitPlayer2 * 100 // rounds)
-
-if maxScore1 > maxScore2:
+#show won and
+if max_score_player1 > max_score_player2:
     print('El ganador es:', player1)
-    if percentagePlayer1 > percentagePlayer2:
+    if percentage_player1 > percentage_player2:
             print('El ganador tuvo mayor porcentaje de aciertos')
-    elif percentagePlayer1 < percentagePlayer2:
+    elif percentage_player1 < percentage_player2:
             print('El perdedor tuvo mayor porcentaje de aciertos')
     else:
-            print('Mismos Porcentajes')
+            print('Con mismos Porcentajes de aciertos')
 
-elif maxScore2 > maxScore1:
+elif max_score_player2 > max_score_player1:
     print('El ganador es:', player2)
-    if percentagePlayer2 > percentagePlayer1:
+    if percentage_player2 > percentage_player1:
             print('El ganador tuvo mayor porcentaje de aciertos')
-    elif percentagePlayer2 < percentagePlayer1:
+    elif percentage_player2 < percentage_player1:
             print('El perdedor tuvo mayor porcentaje de aciertos')
     else:
-            print('Mismos Porcentajes')
+            print('Con mismos Porcentajes de aciertos')
 else:
     if eachScore1 > eachScore2:
         print('\nEl ganador es: ', player1)
@@ -133,21 +126,21 @@ else:
     else:
         print('\n ¡Hay un empate!')
 
-## Estadísticas
+#show percentage
+print('\nPorcentaje de aciertos del jugador: ', player1, '->', percentage_player1, '%')
+print('Porcentaje de aciertos del jugador: ', player2, '->', percentage_player2, '%')
 
 # La cantidad de jugadas realizadas (recordando que una jugada consiste en los turnos de ambos jugadores).
-print('\n La cantidad de jugadas realizadas son: ', rounds)
+print('\nLa cantidad de jugadas realizadas son: ', rounds)
 
 # Si hubo al menos una jugada con puntaje empatado entre ambos jugadores.
-if roundsTied > 0:
-    print('hubo al menos una jugada terminada en empate. En total: ', roundsTied)
+if rounds_tied > 0:
+    print('\nHubo al menos una jugada terminada en empate. En total: ', rounds_tied)
 
 # El puntaje promedio obtenido por jugada por cada jugador.
 
 
 
 
-print('\n Porcentaje de aciertos del jugador: ', player1, '->', percentagePlayer1, '%')
-print('\n Porcentaje de aciertos del jugador: ', player2, '->', percentagePlayer2, '%')
 
 # Si algún jugador ganó en al menos 3 turnos seguidos.
